20b8b9371d43191f029313172c3af32654bae558
daggelito02 <daggelito02@gmail.com>
2021-05-23 11:54:27 +0200

    commiting for the first time on my own

---
+++ dagNgular-app/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero.model';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+titel: string = "Your name";
+selectedHero: Hero | undefined;
+
+ heroes: Hero[] = [
+   {id: 10, name: "Dag"},
+   {id: 12, name: "Petri"},
+   {id: 13, name: "Mattias"},
+   {id: 14, name: "Oskar"},
+   {id: 15, name: "Jocke"}
+  ];
+
+  constructor() { }
+
+  ok(hero: Hero) {
+    this.selectedHero = hero; 
+  }
+
+  ngOnInit(): void {
+  }
+
+}
