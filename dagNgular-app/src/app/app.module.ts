5ccf86c6f847aeb8e4221ea02069e02ca24dd95c
daggelito02 <daggelito02@gmail.com>
2021-05-29 13:59:25 +0200

    dagges-pics

--- dagNgular-app/src/app/app.module.ts
+++ dagNgular-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DaggesPicsComponent } from './dagges-pics/dagges-pics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    DaggesPicsComponent
   ],
   imports: [
     BrowserModule,
---
+++ dagNgular-app/src/app/dagges-pics/dagges-pics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dagges-pics',
+  templateUrl: './dagges-pics.component.html',
+  styleUrls: ['./dagges-pics.component.scss']
+})
+export class DaggesPicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
