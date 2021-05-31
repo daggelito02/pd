2d62597ff133d72e736edf9b397c11e3ed5b2529
daggelito02 <daggelito02@gmail.com>
2021-05-31 10:18:34 +0200

    Oscar super feedback fix

--- dagNgular-app/src/app/app.module.ts
+++ dagNgular-app/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';ut
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { DaggesPicsComponent } from './dagges-pics/dagges-pics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    DaggesPicsComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
--- dagNgular-app/src/app/dagges-pics/dagges-pics.module.ts
+++ dagNgular-app/src/app/dagges-pics/dagges-pics.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DaggesPicsComponent } from './dagges-pics.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    DaggesPicsComponent
   ]
 })
 export class DaggesPicsModule { }
